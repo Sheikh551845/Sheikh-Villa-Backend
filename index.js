d6313877ff618723e4353ae2012ceef10dcb3164
sheikh551845 <sheikh551845@gmail.com>
2023-11-26 19:44:21 +0600

    first commit and AllApartment API added

---
+++ index.js
@@ -0,0 +1,70 @@
+require('dotenv').config()
+const cors = require("cors");
+const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT||8888;
+
+const app= express();
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb://${process.env.DB_User}:${process.env.Use_Password}@ac-dzczvnk-shard-00-00.4kc4xcj.mongodb.net:27017,ac-dzczvnk-shard-00-01.4kc4xcj.mongodb.net:27017,ac-dzczvnk-shard-00-02.4kc4xcj.mongodb.net:27017/?ssl=true&replicaSet=atlas-dehx9a-shard-0&authSource=admin&retryWrites=true&w=majority`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+
+
+
+
+async function run() {
+    try {
+  
+     
+      const SheikhVilla=client.db("SheikhVilla");
+      const AllApartment=SheikhVilla.collection("AllApartment");
+      const AllUsers=SheikhVilla.collection("AllUsers");
+      const Agreement=SheikhVilla.collection("Agreement");
+  
+      app.get("/AllApartment", async(req,res)=>
+      {
+        const cursor = AllApartment.find();
+        const result=await cursor.toArray();
+        res.send(result);
+      });
+
+  
+  
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  
+    
+    } finally {
+      // // Ensures that the client will close when you finish/error
+      // await client.close();
+    }
+  }
+  run().catch(console.dir);
+  
+  
+  
+  
+  
+  app.get('/', (req, res) => {
+      res.send('Hello World!')
+    })
+  
+  app.listen(port, () => {
+      console.log(`My Server is running on port: ${port}`)
+  })
